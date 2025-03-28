146cace94e4f938b482859ebf8becc5dfbe322f3
averagejoestats <gqdusawm@duck.com>
2025-03-28 14:01:11 -0400

    updates

--- lecture_code/lec05_more_factors.R
+++ lecture_code/lec05_more_factors.R
@@ -35,15 +35,15 @@ summary(m3)
 plot( log(dat$total_living_area), log(dat$sale_price) )
 abline( 
     m3$coefficients["(Intercept)"], 
-    m3$coefficients["log(total_living_area)"], col = "magenta"
+    m3$coefficients["log(total_living_area)"], col = "magenta", lwd = 3
 )
 abline( 
     m3$coefficients["(Intercept)"] + m3$coefficients["zip_code63130"], 
-    m3$coefficients["log(total_living_area)"], col = "blue"
+    m3$coefficients["log(total_living_area)"], col = "blue", lwd = 3
 )
 abline( 
     m3$coefficients["(Intercept)"] + m3$coefficients["zip_code63122"], 
-    m3$coefficients["log(total_living_area)"], col = "green"
+    m3$coefficients["log(total_living_area)"], col = "green", lwd = 3
 )
 legend( 
     "topleft", legend = c("63105","63130","63122"), 
@@ -59,7 +59,7 @@ summary(m3a)
 # add the line
 abline( 
     m3a$coefficients["(Intercept)"], 
-    m3a$coefficients["log(total_living_area)"], col = "red"
+    m3a$coefficients["log(total_living_area)"], col = "red", lwd = 3
 )
 
 
@@ -72,13 +72,22 @@ summary( lm( sale_price ~ random, data = dat ) )
 
 # normalize by square feet
 dat$random_per_sq_ft <- dat$random/dat$total_living_area 
-summary( lm( price_per_sq_ft ~ random_per_sq_ft, data = dat ) )
+m_random <-  lm( price_per_sq_ft ~ random_per_sq_ft, data = dat ) 
+summary(m_random)
+
 
 plot( dat$random_per_sq_ft, dat$price_per_sq_ft )
+abline(m_random, col = "magenta")
 # what is going on here?
 
+# There is still a relationship between price per sq ft and 
+# total living area (sq ft), so if you regression price per sq ft
+# on some function of total living area, you will be a significant result, 
+# even for functions total living area that contain randomness
+plot( dat$total_living_area, dat$price_per_sq_ft )
 
-# bedroom analysis
+
+# bedroom analysis 
 summary( 
     lm( log(sale_price) ~ log(total_living_area) + bedrooms, data = dat )
 )
@@ -89,19 +98,95 @@ m4 <- lm( log(sale_price) ~ zip_code + basement, data = dat)
 summary(m4)
 m4$xlevels
 
+# get the table of estimated expected values
+cj <- c(0, m4$coefficients[2:12])
+dk <- c(0,m4$coefficients[13:15])
+
+cj
+dk
+m4$coefficients[1]
+
+# fix the names
+names(cj)[1] <- "zip_code63105"
+names(dk)[1] <- "basementCrawl"
+cj
+dk
+
+tab <- m4$coefficients[1] + outer( cj, dk, FUN = "+" )
+tab
+
+# compare against the averages
+tab2 <- tapply( 
+    log(dat$sale_price), list(dat$zip_code, dat$basement), mean, na.rm = TRUE 
+)
+
+tab
+tab2
+# not quite the same
+
 # let's put some more stuff in the model
-m4 <- lm( 
+m5 <- lm( 
     log(sale_price) ~ 
     zip_code + log(total_living_area) + basement +
     total_acres + log(total_acres) + 
     bedrooms + total_rooms + full_baths +
     half_baths, data = dat
 )
-summary(m4)
+summary(m5)
 
 # residual plot
 plot( m4$fitted.values, m4$residuals )
 
+# build in model plotting function
+plot(m5)
+
+# let's take a look at leverage
+lev <- hatvalues(m5)
+hist(lev, breaks = 100)
+
+ii <- which( lev > 0.10 )
+rows <- names(ii)
+
+dat[rows,]
 
 
+# predicted values and lognormal distribution
+head(dat)
+X <- model.matrix( m5 )
+x4 <- X["4",]
+
+fitval <- sum( x4*m5$coefficients )
+fitval
+crossprod( x4, m5$coefficients )
+m5$fitted.values["4"]
+# all the same
+
+# quartiles
+q25 <- qnorm( 0.25, mean = fitval, sd = sqrt(predvar) )
+q75 <- qnorm( 0.75, mean = fitval, sd = sqrt(predvar) )
+
+predvar <- summary(m5)$sigma^2 + t(x4) %*% vcov(m5) %*% x4
+
+# plot the density and the quartiles
+xx <- seq( 13.5, 15.5, by = 0.01 )
+yy <- dnorm( xx, mean = fitval, sd = sqrt(predvar) )
+plot(xx,yy,type="l")
+abline(v = fitval)
+abline(v = q25 )
+abline(v = q75 )
+
+# expectation of log sale price
+sum( xx*yy*0.01 )
+
+# expectation of sale price = exp(log sale price)
+sum( exp(xx)*yy*0.01 )
+exp( fitval )
+exp( fitval + predvar/2 ) 
+
+# plot of quartiles of sale price = exp(log sale price)
+plot(exp(xx),yy,type="l")
+abline(v = exp(fitval))
+abline(v = exp(q25) )
+abline(v = exp(q75) )
+abline(v = exp(fitval+predvar/2),col = "magenta" )
 
